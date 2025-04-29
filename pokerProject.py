dffdbf53d5efa44a8c5e783c7edbca0316600161
Marco-de-Sá <marcojb.desa@gmail.com>
2025-04-29 12:28:24 +0100

    added a todo and some comments

--- pokerProject.py
+++ pokerProject.py
@@ -9,7 +9,7 @@ class GameDeck:
     The code that handles the hand of the player will most likely be in a different area.
     A brief description of the methods:
 
-    this will probably need rewriting after the class is done
+    todo this will probably need rewriting after the class is done
 
     __init__: will be automatically used on a new object of this class being made
     count_suit: counts the number of cards belonging to a specific suit that remain in the deck
@@ -22,6 +22,7 @@ class GameDeck:
     to_string: I am not sure if this name is in line with python naming practices with this sort
             of method, but basically it takes the deck of cards(list) and transforms it into a string
             which is returned
+
     """
     def __init__(self):
         self.spCard = {1: "ace", 2: "jack", 3: "queen", 4: "king"} # this dictionary contains all types of cards that do not begin with a number
@@ -30,6 +31,8 @@ class GameDeck:
         self.deck = ['ace of clubs', 'ace of spades', 'ace of hearts', 'ace of diamonds', '2 of clubs', '2 of spades', '2 of hearts', '2 of diamonds', '3 of clubs', '3 of spades', '3 of hearts', '3 of diamonds', '4 of clubs', '4 of spades', '4 of hearts', '4 of diamonds', '5 of clubs', '5 of spades', '5 of hearts', '5 of diamonds', '6 of clubs', '6 of spades', '6 of hearts', '6 of diamonds', '7 of clubs', '7 of spades', '7 of hearts', '7 of diamonds', '8 of clubs', '8 of spades', '8 of hearts', '8 of diamonds', '9 of clubs', '9 of spades', '9 of hearts', '9 of diamonds', '10 of clubs', '10 of spades', '10 of hearts', '10 of diamonds', 'jack of clubs', 'jack of spades', 'jack of hearts', 'jack of diamonds', 'queen of clubs', 'queen of spades', 'queen of hearts', 'queen of diamonds', 'king of clubs', 'king of spades', 'king of hearts', 'king of diamonds']
 
     def count_suit(self, target):
+        # this method counts all the cards belonging to the specified suit
+        # warning this method may be vestigial feel free to use it in your code though but please do tell me if you do otherwise I will delete this
         temp = 0 # this variable keeps count of every time a card of target suit is found
         for i in self.deck: # iterates through the deck and checks where the last index is equal to the target String
             if i.split(" ")[-1] == target:
@@ -37,10 +40,12 @@ class GameDeck:
         print(f"There are {temp} cards belonging to {target} remaining in the deck") # prints out the amount of cards belonging to the target suit
 
     def rejoin(self, hand):
+        # this method joins a given list onto the card deck in the class
         for i in range(len(hand)): # takes the iterates through the hand indexes
             self.deck.append(hand.pop()) # pops each hand value into the deck
 
     def deal_cards(self, card_count):
+        # returns the specified amount of cards
         # todo add a way to sort cards before handing them out
         temp = [] # creates a temporary list
         for i in range(int(card_count)): # converts car_count to an integer and then iterates throughout it
@@ -73,6 +78,7 @@ class GameDeck:
         self.deck = tempDeck
 
     def shuffle(self):
+        # shuffles the deck of cards
         for i in range(1000): # iterates the code 1000 times
             ind1, ind2 = randint(0,51), randint(0,51) # randomly generates two indexes that will be swapped later
             # below swaps the two random indexes of the deck variable
