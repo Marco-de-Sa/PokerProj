f93056149cbcd534d7cd1c634ca4a39c77ed706d
Marco-de-Sá <marcojb.desa@gmail.com>
2025-05-06 13:43:22 +0100

    refactor GameDeck initialization to use lists for card types and suits

--- pokerProject.py
+++ pokerProject.py
@@ -25,8 +25,8 @@ class GameDeck:
 
     """
     def __init__(self):
-        self.spCard = {1: "ace", 2: "jack", 3: "queen", 4: "king"} # this dictionary contains all types of cards that do not begin with a number
-        self.suit = {1: "clubs", 2: "spades", 3: "hearts", 4: "diamonds"} # a dictionary that stores all possible suits of cards
+        self.spCard = ["ace", "jack", "queen", "king"] # this dictionary contains all types of cards that do not begin with a number
+        self.suit = ["clubs", "spades", "hearts", "diamonds"] # a dictionary that stores all possible suits of cards
         # declares a list called deck that holds every type of card
         self.deck = ['ace of clubs', 'ace of spades', 'ace of hearts', 'ace of diamonds', '2 of clubs', '2 of spades', '2 of hearts', '2 of diamonds', '3 of clubs', '3 of spades', '3 of hearts', '3 of diamonds', '4 of clubs', '4 of spades', '4 of hearts', '4 of diamonds', '5 of clubs', '5 of spades', '5 of hearts', '5 of diamonds', '6 of clubs', '6 of spades', '6 of hearts', '6 of diamonds', '7 of clubs', '7 of spades', '7 of hearts', '7 of diamonds', '8 of clubs', '8 of spades', '8 of hearts', '8 of diamonds', '9 of clubs', '9 of spades', '9 of hearts', '9 of diamonds', '10 of clubs', '10 of spades', '10 of hearts', '10 of diamonds', 'jack of clubs', 'jack of spades', 'jack of hearts', 'jack of diamonds', 'queen of clubs', 'queen of spades', 'queen of hearts', 'queen of diamonds', 'king of clubs', 'king of spades', 'king of hearts', 'king of diamonds']
 
