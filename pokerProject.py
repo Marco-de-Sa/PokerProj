3a093fd984b02d75335e35dc73d4a8f9715425ac
Marco-de-Sá <marcojb.desa@gmail.com>
2025-05-24 17:02:20 +0100

    refactor sort_deck method to implement binary insertion sort and remove WIP comment

--- pokerProject.py
+++ pokerProject.py
@@ -61,7 +61,6 @@ class GameDeck:
         this method sorts the deck of cards in a specific order using a sorting algorithm chosen by the user
         """
 
-        # beware code here is WIP and not ready for use
         tempDeck = [] # creates a temporary list
         if optional == list:
             tempDeck = optional # makes a copy of the optional list to sort
@@ -134,13 +133,40 @@ class GameDeck:
             heapSort(diamond)
 
         elif sortingType == "binary insertion":
+            def binary_search(arr, val, start, end):
+    
+                # we need to distinguish whether we 
+                # should insert before or after the 
+                # left boundary. imagine [0] is the last 
+                # step of the binary search and we need 
+                # to decide where to insert -1
+                if start == end:
+                    if int(arr[start].split(" ")[0]) > int(val.split(" ")[0]):
+                        return start
+                    else:
+                        return start+1
+
+                # this occurs if we are moving 
+                # beyond left's boundary meaning 
+                # the left boundary is the least 
+                # position to find a number greater than val
+                if start > end:
+                    return start
+
+                mid = (start+end)//2
+                if int(arr[mid].split(" ")[0]) < int(val.split(" ")[0]):
+                    return binary_search(arr, val, mid+1, end)
+                elif int(arr[mid].split(" ")[0]) > int(val.split(" ")[0]):
+                    return binary_search(arr, val, start, mid-1)
+                else:
+                    return mid
+
+
             def insertion_sort(arr):
                 for i in range(1, len(arr)):
-                    for j in range(i):
-                        if int(arr[i].split(' ')[0]) < int(arr[j].split(' ')[0]):
-                            temp = arr[i]
-                            arr[i] = arr[j]
-                            arr[j] = temp
+                    val = arr[i]
+                    j = binary_search(arr, val, 0, i-1)
+                    arr = arr[:j] + [val] + arr[j:i] + arr[i+1:]
                 return arr
             
             # parses each card list into the insertion sort function
