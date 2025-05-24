0d08b85a43ef724444cc7f53198ad491d6f4eeac
Marco-de-Sá <marcojb.desa@gmail.com>
2025-05-24 19:04:14 +0100

    refactor sort_deck method documentation for clarity and remove unnecessary comments

--- pokerProject.py
+++ pokerProject.py
@@ -15,8 +15,7 @@ class GameDeck:
     count_suit: counts the number of cards belonging to a specific suit that remain in the deck
     rejoin: will take a list of cards provided by the player and rejoin them into the deck
     deal_cards: deals out the specified number of cards in list form
-    sort_deck: is at the moment non-functional, but it is planned to have a way for the player to
-            select what sorting method will be used
+    sort_deck: allows the user to sort the deck of cards in a specific order using a sorting algorithm chosen by the user
     shuffle: shuffles the deck of cards using the fisher yates method
     __str__: this method overwrites the default __str__ method to instead return the string below
             this is used to print out the deck of cards in a string format
@@ -25,8 +24,6 @@ class GameDeck:
     def __init__(self):
         # declares a list called deck that holds every type of card
         self.deck = ['ace of clubs', 'ace of spades', 'ace of hearts', 'ace of diamonds', '2 of clubs', '2 of spades', '2 of hearts', '2 of diamonds', '3 of clubs', '3 of spades', '3 of hearts', '3 of diamonds', '4 of clubs', '4 of spades', '4 of hearts', '4 of diamonds', '5 of clubs', '5 of spades', '5 of hearts', '5 of diamonds', '6 of clubs', '6 of spades', '6 of hearts', '6 of diamonds', '7 of clubs', '7 of spades', '7 of hearts', '7 of diamonds', '8 of clubs', '8 of spades', '8 of hearts', '8 of diamonds', '9 of clubs', '9 of spades', '9 of hearts', '9 of diamonds', '10 of clubs', '10 of spades', '10 of hearts', '10 of diamonds', 'jack of clubs', 'jack of spades', 'jack of hearts', 'jack of diamonds', 'queen of clubs', 'queen of spades', 'queen of hearts', 'queen of diamonds', 'king of clubs', 'king of spades', 'king of hearts', 'king of diamonds']
-
-###########################################################################################################
 
     def count_suit(self, target):
         """this method counts all the cards belonging to the specified suit"""
@@ -37,14 +34,10 @@ class GameDeck:
                 temp += 1
         print(f"There are {temp} cards belonging to {target} remaining in the deck") # prints out the amount of cards belonging to the target suit
 
-###########################################################################################################
-
     def rejoin(self, hand):
         """this method joins a given list onto the card deck in the class"""
         for i in range(len(hand)): # takes the iterates through the hand indexes
             self.deck.append(hand.pop()) # pops each hand value into the deck
-
-###########################################################################################################
 
     def deal_cards(self, card_count):
         """returns the specified amount of cards"""
@@ -53,8 +46,6 @@ class GameDeck:
         for i in range(int(card_count)): # converts car_count to an integer and then iterates throughout it
             temp.append(self.deck.pop()) # appends the specified amount(from card_count) to the temp list by popping them from the deck
         return temp # returns the temp list
-    
-###########################################################################################################
 
     def sort_cards(self, optional):
         """
@@ -252,7 +243,7 @@ class GameDeck:
             quickSort(heart, 0, len(heart) - 1)
             quickSort(club, 0, len(club) - 1)
             quickSort(diamond, 0, len(diamond) - 1)
-            
+
         else:
             print("Invalid sorting type, please try again")
         
@@ -286,13 +277,9 @@ class GameDeck:
         
         return tempDeck # returns the sorted deck
 
-###########################################################################################################
-
     def set_deck(self, newDeck):
         """this method makes the deck equal to the newDeck provided"""
         self.deck = newDeck
-
-###########################################################################################################
 
     def shuffle(self):
         """shuffles the deck of cards"""
@@ -303,8 +290,6 @@ class GameDeck:
             shuffled.append(self.deck[k])
             self.deck.pop(k)
         self.deck = shuffled
-
-###########################################################################################################
 
     def __str__(self):
         """this method overwrites the default __str__ method to instead return the string below"""
